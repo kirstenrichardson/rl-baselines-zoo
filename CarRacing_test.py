28251a10af8899d2b31021946175aa4df0858801
b3024896 <b3024896@18scompl028.ncl.ac.uk>
2020-04-17 17:08:28 +0100

    no changes, just added alternative way of creating env to test script and commented out incase go back to

--- CarRacing_test.py
+++ CarRacing_test.py
@@ -2,8 +2,19 @@ import gym
 from stable_baselines.common.policies import CnnPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines import PPO2
+#from gym_CarRacing.envs.CarRacing_env import CarRacingEnv, original_reward_callback
 
-#env = gym.make('gym_CarRacing:CarRacing-v1') 
+#env = lambda :  CarRacingEnv(
+    #grayscale=1,
+    #show_info_panel=0,
+    #discretize_actions="hard",
+    #frames_per_state=4,
+    #num_lanes=1,
+    #num_tracks=1,
+    #reward_fn=original_reward_callback
+    #)
+#env = DummyVecEnv([env])
+
 env = DummyVecEnv([lambda: gym.make('gym_CarRacing:CarRacing-v1')])
 
 model = PPO2.load('car_racing_weights.pkl')
