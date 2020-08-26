fe7223f8b70fd384f8045690662332f5ae3bc350
b3024896 <b3024896@18scompl028.ncl.ac.uk>
2020-08-26 08:40:10 +0100

    something to train.py but not sure what, look at file

--- train.py
+++ train.py
@@ -35,7 +35,7 @@ from stable_baselines.common import set_global_seeds
 from stable_baselines.common.cmd_util import make_atari_env
 from stable_baselines.common.vec_env import VecFrameStack, SubprocVecEnv, VecNormalize, DummyVecEnv
 from stable_baselines.common.noise import AdaptiveParamNoiseSpec, NormalActionNoise, OrnsteinUhlenbeckActionNoise
-from stable_baselines.ppo2.ppo2 import constfn
+from stable_baselines.common.schedules import constfn
 from stable_baselines.common.callbacks import CheckpointCallback                                                            ########## I ADDED
 
 from utils import make_env, ALGOS, linear_schedule, get_latest_run_id, get_wrapper_class, find_saved_model
